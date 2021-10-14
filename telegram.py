fbbe4cc70943f4d782321c1c00ef2e5ead6d1df5
kattelasaikrishna <67508837+kattelasaikrishna@users.noreply.github.com>
2021-10-14 21:52:06 +0530

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,45 @@
+pip install adafruit-io
+from Adafruit_IO import Client
+aio = Client('kattelasaikrishna', 'aio_ldck71BnIIqkX6zO5O9S6C6liJfJ')
+pip install python-telegram-bot==13.0 --quiet
+
+from telegram.ext import Updater, MessageHandler, Filters
+
+def demo1(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning on the Light')
+  aio.send('light',1)
+
+def demo2(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning off the Light')
+  aio.send('light',0)
+
+def demo3(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning on the Fan')
+  aio.send('fan',1)
+
+def demo4(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning off the Fan')
+  aio.send('fan',0)
+
+def main(bot,update):
+  a= bot.message.text.lower()
+  if a=='lights on' or a==' turn on lights' or a==' turn lights on' or a==' turn on light' or a=='on light' or a=='light on' or a=='on lights' :
+    demo1(bot,update)
+  elif a=='lights off' or a==' turn off lights' or a==' turn lights off' or a==' turn off light' or a=='off light' or a=='light off' or a=='off lights' :
+    demo2(bot,update)
+  elif a=='lights off' or a==' turn off lights' or a==' turn lights off' or a==' turn off light' or a=='off light' or a=='light off' or a=='on fans' :
+    demo3(bot,update)
+  elif a=='fans off' or a==' turn off fans' or a=='turn fans off' or a==' turn fan off ' or a=='off fan' or a=='fan off'  or a=='off fans' :
+    demo4(bot,update)
+
+
+bot_token = '2046009698:AAErk31YPE8ujNPihuO5HTDZplbpSNk5WiU'
+u = Updater(bot_token,use_context=True)
+dp=u.dispatcher
+dp.add_handler(MessageHandler(Filters.text,main))
+u.start_polling()
+u.idle()
